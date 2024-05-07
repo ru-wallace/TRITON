4267f27d6198ee0654d0bec9b9178979b5de7f46
Ru W <ru@wallace.scot>
2024-05-07 12:54:32 +0100

    run with continuous mode

--- python_scripts/get_sharpness.py
+++ python_scripts/get_sharpness.py
@@ -16,81 +16,92 @@ class Sharpness_Tool:
                     
         self.count = 0
 
-        self.device.set_all_to_manual()
         self.device.stop_acquisition()
-        self.device.node("AcquisitionMode").SetCurrentEntry("SingleFrame")
-        self.device.load_settings("Default")
-        self.exposure_time = self.device.exposure_time(microseconds=150)
 
+        self.device.load_settings("Default")
+        self.device.node("PixelFormat").SetCurrentEntry("Mono8")
+        self.device.node("AcquisitionMode").SetCurrentEntry("Continuous")
+        self.exposure_time = self.device.exposure_time(microseconds=150)
+        self.device.node("ExposureAuto").SetCurrentEntry("Continuous")
+        self.device.start_acquisition()
+        self.last_error = None
         self.error_count = 0
         self.cam_active = False
         self.error = False
+        self.ROI = None
+        self.auto = False
         
         while self.error_count < 5:
             
             try:
                 self.error = False
-                self.auto = self.count == 0
-                
-                
-                if not self.cam_active:
-                    try:
-                        
-                        cam_thread = threading.Thread(target=self.start_cam_thread, args=(self.auto,))
-                        cam_thread.start()
-                        self.count += 1
-                        if self.count > 5:
-                            self.count = 0
-                    except KeyboardInterrupt:
-                        print("")
-                        print("Exiting...")
-                        break
-                    except:
-                        self.error = True
-                
-                if not self.error:
-                    self.error_count = 0
+                #self.auto = self.count == 0
+
+                self.capture()
+                self.count += 1
+                if self.count > 40:
+                    self.count = 0
+
+            
                 
             except KeyboardInterrupt:
                 print("")
                 print("Exiting...")
                 break
-            except:
+            except Exception as e:
+                traceback.print_exc(e)
                 self.error = True
+                self.last_error = e
             finally:
+                print("error count: ", self.error_count)
                 self.error_count += self.error
                 if self.error_count > 4:
                     print("")
                     print("Too many errors. Quitting.")
+                    traceback.print_exc(self.last_error)
                     break
                 pass
             
         print("Closing self.device connection")   
+        
+        
         self.device.stop_acquisition()
         self.device.close_connection()
     
-    def start_cam_thread(self, auto=False):
+    
+    
+    
+    def capture(self, auto=False):
         try:
-            if self.cam_active or self.error_count > 4:
-                return
-            
-            self.cam_active = True
-            
-            if auto:
-                self.device.capture_auto_exposure()
-                self.exposure_time = self.device.exposure_time()
-
-            print(f"Sharpness: {round(self.device.sharpness_test(), 2)} Integration Time: {self.exposure_time/1000000}s ({self.exposure_time}us)", end=f"   \r")
-            
-            
+            #print(f"Capturing - Exposure time: {self.device.exposure_time()/1e6}s")
+            sys.stdout.flush()
+            image = self.device.capture_frame(return_type=ids_interface.Resources.IPL_IMAGE)
+            sharpness_thread = threading.Thread(self.print_sharpness, args=(image,))
+            sys.stdout.flush()
+            sharpness_thread.start()
         except KeyboardInterrupt:
             self.error = True
             self.error_count = 10
         except Exception as e:
             traceback.print_exc(e)
             self.error = True
-        finally:
-            self.cam_active = False
+            self.last_error = e
+
+            
+    def print_sharpness(self, image):
+        try:
+            print("Hello", flush=True)
+            if not self.ROI:
+                self.ROI = ids_interface.make_ROI_start_centre(image, size=600)
+                    
+            sharpness = ids_interface.calculate_sharpness(image, self.ROI)
+            print(f"Sharpness: {round(sharpness, 2)} Integration Time: {self.exposure_time/1000000}s ({self.exposure_time}us)", end=f"   \r", flush=True)
+            sys.stdout.flush()
+        except Exception as e:
+            self.last_error = e
+            self.error_count +=1
+                
+
 if __name__ == "__main__":
     try:
         Sharpness_Tool()
